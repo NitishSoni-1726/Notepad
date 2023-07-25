8ebc62b03c791d660bede382337c135765e7feb9
Nitish Soni <nitishsoni890@gmail.com>
2023-07-25 12:17:12 +0530

    created add note button

--- components/AddNotes.js
+++ components/AddNotes.js
@@ -1,12 +1,17 @@
+import * as React from "react";
 import { TouchableHighlight } from "react-native";
 import { FAB } from "react-native-elements";
 
-export default function AddNotes() {
-  function handleAddNotes() {}
+export default function AddNotes({ navigation }) {
+  function handleAddNotes() {
+    navigation.push("AddNote");
+  }
   return (
     <TouchableHighlight>
       <FAB
-        onPress={handleAddNotes}
+        onPress={() => {
+          navigation.push("AddNote");
+        }}
         title="+"
         color="#27374D"
         placement="right"
