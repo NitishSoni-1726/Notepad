73ec043d97105c12187864fea87bd9f41843954c
Nitish Soni <nitishsoni890@gmail.com>
2023-07-25 12:16:35 +0530

    added hard coded notes

--- components/NotesData.js
+++ components/NotesData.js
@@ -7,12 +7,12 @@ export const notes = [
   { title: "Second", note: "Lorem, ipsum.", date: "10-10-2023" },
   {
     title: "Third",
-    note: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos iusto,molestiae nulla quo neque ea? Id cum inventore dolore consequatur perspiciatis sed ducimus suscipit consectetur? Excepturi, quidem tenetur necessitatibus id accusamus mollitia quae eum at alias ipsa? Aliquam repudiandae assumenda animi architecto autem, ex dolores quis obcaecati dolore, neque id!",
+    note: "Lorem ipsum dolor.\nEos iusto,molestiae nulla quo neque ea? Id cum inventore dolore consequatur perspiciatis sed ducimus suscipit consectetur? Excepturi, quidem tenetur necessitatibus id accusamus mollitia quae eum at alias ipsa? Aliquam repudiandae assumenda animi architecto autem, ex dolores quis obcaecati dolore, neque id!",
     date: "12-07-2023",
   },
   {
     title: "Fourth",
-    note: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, voluptas?",
+    note: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio qua in voluptate tempora aperiam cum! Labore deleniti qui a, itaque voluptatibus nemo et beatae harum unde. Sapiente perspiciatis autem, laborum inventore nisi doloremque dolorum similique qui, mollitia amet, tenetur perferendis dolores iste quos. Esse iusto laboriosam commodi quidem excepturi reprehenderit ad odio amet atque fugit modi numquam architecto repudiandae optio aspernatur fugiat obcaecati consectetur ut nihil voluptate ipsum mollitia, officiis laborum hic. Nostrum assumenda hic obcaecati non inventore? Unde laudantium itaque tempora dignissimos tenetur eveniet eius deleniti quibusdam quae. Possimus nam, mollitia rerum facere totam ipsum quas magnam aspernatur eum tenetur dolore iste, inventore molestias! Earum dolorum aperiam, repellat assumenda nisi odit quas! Ad accusantium, reiciendis magnam earum placeat quia error deserunt optio doloremque repellat a, enim vero tempore dolorem impedit. Error rem debitis id reprehenderit iure eligendi pariatur quae, molestias tenetur similique facere dolore magni quis sint quod excepturi nisi, natus repellendus fuga labore eaque dolor necessitatibus voluptate. Pariatur eaque, dolores aliquid cupiditate ea ullam sint unde, maxime reiciendis,",
     date: "20-07-2023",
   },
   {
