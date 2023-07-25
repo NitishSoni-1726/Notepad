9a68a28782460553a81b8bba7d1f7b6878fcaef2
Nitish Soni <nitishsoni890@gmail.com>
2023-07-25 12:16:50 +0530

    created notes list

--- components/NotesList.js
+++ components/NotesList.js
@@ -1,7 +1,10 @@
-import { ActivityIndicator, Text, View } from "react-native";
+import * as React from "react";
+import { ActivityIndicator, ScrollView, Text, View } from "react-native";
 import Notes from "./Notes";
 import { notes } from "./NotesData";
 import { useEffect, useState } from "react";
+import AddNotes from "./AddNotes";
+import Navbar from "./Navbar";
 export default function NoteList(props) {
   const [data, setData] = useState({
     loading: "not_fetched",
@@ -34,41 +37,56 @@ export default function NoteList(props) {
       style={{
         display: "flex",
         alignItems: "center",
+        justifyContent: "center",
         width: "100%",
         height: "100%",
         backgroundColor: "#7F8487",
+        paddingBottom: 7,
       }}
     >
-      {data.loading === "not_fetched" ? (
-        <View
-          style={{
-            marginTop: 10,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            padding: 50,
-            backgroundColor: "#7F9090",
-            width: "100%",
-            height: "100%",
-          }}
-        >
-          <ActivityIndicator size="large" color="white" />
-          <Text style={{ color: "white", marginTop: 10, fontSize: 15 }}>
-            Loading Notes...
-          </Text>
-        </View>
-      ) : (
-        filteredNotes.map((item, index) => {
-          return (
-            <Notes
-              key={index}
-              note={item.note}
-              title={item.title}
-              date={item.date}
-            />
-          );
-        })
-      )}
+      <Navbar setSearchInput={props.setSearchInput} />
+      <ScrollView style={{ width: "100%", paddingLeft: 10 }}>
+        {data.loading === "not_fetched" ? (
+          <View
+            style={{
+              marginTop: 10,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: 50,
+              backgroundColor: "#7F9090",
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            <ActivityIndicator size="large" color="white" />
+            <Text style={{ color: "white", marginTop: 10, fontSize: 15 }}>
+              Loading Notes...
+            </Text>
+          </View>
+        ) : (
+          filteredNotes.map((item, index) => {
+            return (
+              <Notes
+                key={index}
+                note={item.note}
+                title={item.title}
+                date={item.date}
+              />
+            );
+          })
+        )}
+      </ScrollView>
+      <View
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "flex-end",
+        }}
+      >
+        <AddNotes />
+      </View>
     </View>
   );
 }
